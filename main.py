9db43918093987ce992df9096f8203c2ce410524
Zzzzz888 <116314149+Zzzzz888@users.noreply.github.com>
2023-01-19 23:02:31 +0000

    babygirl

--- main.py
+++ main.py
@@ -12,12 +12,29 @@ while boolean:
   user_input = input("Enter a choice (Rock, Paper, Scissor): ").lower()
   print("You chose:" + user_input)
 
+  
+  
+  boolean=False
+ 
+  
+  
   choice = ["Rock", "Paper", "Scissor"]
+  outcome=random.choice(choice)
   computer = random.choices(choice)
-  print([user_input], "vs", [computer])
+  print(user_input, "vs", outcome)
 
+  
+  
   if user_input == computer:
-    print("Both Players Selected ([user_input]). It's a tie")
+    print("I mean its a unwanted result but it could be worse:)")
+
+  elif user_input>computer:
+    print("YOU ARE VICTORIUS! YOU WILL HAVE ALL THE FULLIMENT IN LIFE THAT YOU DESERVE")
+  elif user_input<computer:
+    print("YOU TAKE Ls! YOU WILL NEVER GET RID OF YOUR FAILMENT HERE")
+      
+    
+    
   
-  
+
   
