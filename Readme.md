74aec9bfb90e0a25ca36e0b2e10f2e7269eacd1f
Zzzzz888 <116314149+Zzzzz888@users.noreply.github.com>
2023-01-24 23:00:09 +0000

    put in rules

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# How to Create a Rock Paper Scissors
+## Instructions 
+- Press Run TO Begin
+
+- Write Rock, Paper, or Scissor to begin
+
+## Rules
+- Rock beats Scissor
+
+- Paper beats Rock
+
+- Scissor beats Paper
+
+- If Same It's A Tie
+
+- Repeat Game For More
--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 ## Requirements
 import random
 
-Rock = "Rock"
-Paper = "Paper"
-Scissor = "Scissor"
+Rock = "rock"
+Paper = "paper"
+Scissor = "scissor"
 
 boolean=True
 while boolean:
@@ -19,21 +19,34 @@ while boolean:
   
   
   choice = ["Rock", "Paper", "Scissor"]
-  outcome=random.choice(choice)
-  computer = random.choices(choice)
+  outcome=random.choice(choice).lower()
+  
   print(user_input, "vs", outcome)
 
   
   
-  if user_input == computer:
-    print("I mean its a unwanted result but it could be worse:)")
-
-  elif user_input>computer:
-    print("YOU ARE VICTORIUS! YOU WILL HAVE ALL THE FULLIMENT IN LIFE THAT YOU DESERVE")
-  elif user_input<computer:
-    print("YOU TAKE Ls! YOU WILL NEVER GET RID OF YOUR FAILMENT HERE")
+  if user_input == outcome:
+      print("I mean its a unwanted result but it could be worse:)")
+  
+  elif user_input == Rock and outcome == Scissor:
+      print("\nYOU ARE VICTORIUS! YOU WILL HAVE ALL THE FULLIMENT IN LIFE THAT YOU DESERVE WITH THE EMPOWERMENT YOU'VE GAINED")
       
+  elif user_input == Paper and outcome == Scissor:
+      print("\nYOU TAKE Ls! YOU WILL NEVER GET RID OF YOUR FAILMENT HERE")
+     
+  elif user_input == Scissor and outcome == Paper:
+      print("\nYOU ARE VICTORIUS! YOU WILL HAVE ALL THE FULLIMENT IN LIFE THAT YOU DESERVE WITH THE EMPOWERMENT YOU'VE GAINED")
+     
+  elif user_input == Paper and outcome == Rock:
+      print("\nYOU ARE VICTORIUS! YOU WILL HAVE ALL THE FULLIMENT IN LIFE THAT YOU DESERVE WITH THE EMPOWERMENT YOU'VE GAINED")
+      
+  elif user_input == Rock and outcome == Paper:
+      print("\nYOU TAKE Ls! YOU WILL NEVER GET RID OF YOUR FAILMENT HERE")
     
+  elif user_input == Scissor and outcome == Rock:
+      print("\nYOU TAKE Ls! YOU WILL NEVER GET RID OF YOUR FAILMENT HERE")
+    
+      
     
   
 
